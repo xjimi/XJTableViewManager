47aefa60f38cbe0cb977fa459753946bed47465f
XJIMI <fn5128@gmail.com>
2018-12-20 13:15:52 +0800

    add Demo and .gitignore

---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by XJIMI on 2018/12/20.
+//  Copyright © 2018 XJIMI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end

d3a7d620acc17d947b1bd79df475a63ad7fca847
XJIMI <fn5128@gmail.com>
2018-12-20 16:29:33 +0800

    remove delegate

--- XJTableViewManager/XJTableViewManager.h
+++ XJTableViewManager/XJTableViewManager.h
@@ -8,7 +8,6 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "XJTableViewDelegate.h"
 #import "XJTableViewDataModel.h"
 #import "XJTableViewHeaderModel.h"
 #import "XJTableViewFooterModel.h"
@@ -16,6 +15,41 @@
 #import "XJTableViewHeader.h"
 #import "XJTableViewFooter.h"
 #import "XJTableViewCell.h"
+
+@class XJTableViewManager;
+
+@protocol XJTableViewDelegate < NSObject >
+
+@optional
+- (NSInteger)xj_tableView:(XJTableViewManager *)tableView numberOfRowsInSection:(NSInteger)section;
+
+- (CGFloat)xj_tableView:(XJTableViewManager *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+- (UITableViewCell *)xj_tableView:(XJTableViewManager *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (void)xj_tableView:(XJTableViewManager *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (BOOL)xj_tableView:(XJTableViewManager *)tableView canEditRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (UIView *)xj_tableView:(XJTableViewManager *)tableView viewForHeaderInSection:(NSInteger)section;
+
+- (UITableViewCellEditingStyle)xj_tableView:(XJTableViewManager *)tableView
+              editingStyleForRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (void)xj_tableView:(XJTableViewManager *)tableView
+  commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+   forRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (void)xj_tableView:(XJTableViewManager *)tableView didEndEditingRowAtIndexPath:(nullable NSIndexPath *)indexPath;
+
+- (BOOL)xj_tableView:(XJTableViewManager *)tableView canMoveRowAtIndexPath:(nonnull NSIndexPath *)indexPath;
+
+- (void)xj_tableView:(XJTableViewManager *)tableView
+  moveRowAtIndexPath:(nonnull NSIndexPath *)sourceIndexPath
+         toIndexPath:(nonnull NSIndexPath *)destinationIndexPath;
+
+@end
+
 
 typedef void (^XJTableViewCellForRowBlock) (XJTableViewCellModel *cellModel, XJTableViewCell *cell, NSIndexPath *indexPath);
 
